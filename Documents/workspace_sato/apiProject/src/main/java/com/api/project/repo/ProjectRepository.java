11c46a400da60ab333808dd6a173853e889e6e81
SatoRihwa <95674248+SatoRihwa@users.noreply.github.com>
2023-01-26 18:04:27 +0900

    Delete Documents directory

--- Documents/workspace_sato/apiProject/src/main/java/com/api/project/repo/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.project.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.project.bean.Project;
-
-public interface ProjectRepository  extends JpaRepository<Project, Integer>{
-
-}
